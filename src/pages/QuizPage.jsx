02a8c91c01e2d1dd095e48062b81e009120573bd
Abhishek <abhisheksharma7124@gmail.com>
2024-10-06 06:55:48 +0530

    added QuizPage

---
+++ src/pages/QuizPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Quiz from '../components/Quiz'
+
+function QuizPAge() {
+  return (
+    <div className="bg-gray-900">
+      <Navbar/>
+        <Quiz/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default QuizPAge
